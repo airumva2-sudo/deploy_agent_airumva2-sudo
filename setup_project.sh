692cc45ffd3fe2ded0cf16f4ed4fda94744a5488
airumva2-sudo <a.irumva2@alustudent.com>
2026-06-16 14:13:26 -0700

    Newest update

--- setup_project.sh
+++ setup_project.sh
@@ -5,8 +5,9 @@ trap cleanup SIGINT
 cleanup() {
         echo "Interrupt detected. Creating archive."
         tar -czf "attendance_tracker_${project_name}_archive.tar.gz" "attendance_tracker_${project_name}"
-        rm -rf "attendance_tracker_$project_name"
+      
         echo "Archive created successfully.Now deleting the incomplete workspace"
+	rm -rf "attendance_tracker_${project_name}"
         echo "Incomplete project directory removed."
         exit 0
 }
@@ -17,7 +18,9 @@ read project_name
 if [ -d "attendance_tracker_$project_name" ]; then
 	echo "Directory already exists."
 	exit 1
+
 fi
+
 mkdir "attendance_tracker_$project_name"
 echo "Main folder created successfully!"
 mkdir "attendance_tracker_$project_name/Helpers"
@@ -30,22 +33,23 @@ cp config.json "attendance_tracker_$project_name/Helpers/"
 cp reports.log "attendance_tracker_$project_name/reports/"
 
 read -p "Do you want to update the attendance thresholds(Y/N)?" update_thresholds
-if [[ "$update_thresholds"=~ ^[Yy]$ ]]; then
+if [[ "$update_thresholds" =~ ^[Yy]$ ]]; then
         read -p "Enter warning percentage:" warning_marks
         read -p "Enter failure percentage:" fail_marks
-        sed -i "s/\"warning\": *[0-9]*/\"warning\": $warning_marks/" "attendance_tracker_$project_name/Helpers/config.json"
-        sed -i "s/\"failure\": *[0-9]*/\"failure\": $fail_marks/" "attendance_tracker_$project_name/Helpers/config.json"
+        sed -i "s/\"warning\": [0-9]*/\"warning\": $warning_marks/" "attendance_tracker_$project_name/Helpers/config.json"
+        sed -i "s/\"failure\": [0-9]*/\"failure\": $fail_marks/" "attendance_tracker_$project_name/Helpers/config.json"
         echo "Thresholds updated for warning ($warning_marks) and failure ($fail_marks)"
 else
         echo "Now using the default thresholds: warning (75%) and failure (50%)"
 fi
 
 echo "Checking Python installation."
-if python3 --version >/dev/null 2>&1; then
+if python3 --version >/dev/null 2>&1;
+then
         echo "Python3 is installed"
         python3 --version
 else        
-	echo "Warning: Python3 is not installed"
+-	echo "Warning: Python3 is not installed"
 fi
 if [ -f "attendance_tracker_$project_name/attendance_checker.py" ] &&
    [ -f "attendance_tracker_$project_name/Helpers/assets.csv" ] &&
