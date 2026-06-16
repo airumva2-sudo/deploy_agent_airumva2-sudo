b07cb918c5e848ac4514511944050dca2949fa26
airumva2-sudo <a.irumva2@alustudent.com>
2026-06-16 02:28:03 -0700

    Implement config updates using sed

--- setup_project.sh
+++ setup_project.sh
@@ -14,4 +14,14 @@ cp attendance_checker.py "attendance_tracker_$project_name/"
 cp assets.csv "attendance_tracker_$project_name/Helpers/"
 cp config.json "attendance_tracker_$project_name/Helpers/"
 cp repots.log "attendance_tracker_$project_name/repots/"
+read -p "Do you want to update the attendance thresholds(Y/N)?" update_thresholds
+if [[ "$update_thresholds"=~^ [Yy]$ ]]; then
+        read -p "Enter warning percentage:" warning_marks
+        read -p "Enter failure percentage:" fail_marks
+        sed -i "s/\"warning\": *[0-9]*/\"warning\": $warning_marks/" "attendance_tracker_$project_name/Helpers/config.json"
+        sed -i "s/\"failure\": *[0-9]*/\"failure\": $fail_marks/" "attendance_tracker_$project_name/Helpers/config.json"
+        echo "Thresholds updated for warning ($warning_marks) and failure ($fail_marks)"
+else
+        echo "Now using the default thresholds: warning (75%) and failure (50%)"
+fi
 
