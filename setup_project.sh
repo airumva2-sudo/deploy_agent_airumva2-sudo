cb7e8e3fff0fb96ddc919031c0f559d86a28590d
airumva2-sudo <a.irumva2@alustudent.com>
2026-06-16 02:25:56 -0700

    Add automated directory bootstrap script

--- setup_project.sh
+++ setup_project.sh
@@ -10,3 +10,8 @@ echo "Main folder created successfully!"
 mkdir "attendance_tracker_$project_name/Helpers"
 mkdir " attendance_tracker_$project_name/reports"
 echo "Project structure created successfully"
+cp attendance_checker.py "attendance_tracker_$project_name/"
+cp assets.csv "attendance_tracker_$project_name/Helpers/"
+cp config.json "attendance_tracker_$project_name/Helpers/"
+cp repots.log "attendance_tracker_$project_name/repots/"
+
